87b488dd3ceb0e7bb9a1458e20aeba6324e07371
Quentin Veyret <quent1.veyret@gmail.com>
2019-01-26 15:40:39 +0100

    init using angular-cli, with angular-material

---
+++ front/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+import { AngularMaterialModule } from './angular-material/angular-material.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
