0dc77ef07a2993351c85bf3d20eb0db422a2a27c
Valentin Coudert <vallenain@free.fr>
2019-01-27 18:40:16 +0100

    added basic navbar

---
+++ admin/main.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+import datetime
+
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+
+
+@app.route('/<path:path>')
+def send_static_resource(path):
+    return send_from_directory('dist/admin', path)
+
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    # Flask's development server will automatically serve static files in
+    # the "static" directory. See:
+    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
+    # App Engine itself will serve those files as configured in app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
--- admin/src/app/angular-material/angular-material.module.ts
+++ admin/src/app/angular-material/angular-material.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import {
+MatButtonModule,
+MatCheckboxModule ,
+MatIconModule,
+MatTooltipModule,
+MatToolbarModule
+} from '@angular/material';
 
 const materialModules = [
   MatButtonModule,
-  MatCheckboxModule
+  MatCheckboxModule,
+  MatIconModule,
+  MatTooltipModule,
+  MatToolbarModule
 ];
 
 @NgModule({
