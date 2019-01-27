ba7c51b67eed16d9793e9a939502ed3a111fdd0c
Valentin Coudert <vallenain@free.fr>
2019-01-27 18:40:16 +0100

    added basic navbar

--- admin/src/app/app.module.ts
+++ admin/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AngularMaterialModule } from './angular-material/angular-material.module';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
