368aa629e1f81c16435ec9ae617b65cf76bbc033
Jérémi Le Bourhis <jeremilebourhis@contentsquare.com>
2019-04-25 06:58:23 +0200

    rename the angular app from big-trekker to front

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'big-trekker'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('big-trekker');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to big-trekker!');
-  });
 });
